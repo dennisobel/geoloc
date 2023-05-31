7c9b796f22375b0605af93f644e47b9ad32369af
dennisobel <ogembodenis2016@gmail.com>
2023-05-31 12:16:12 +0300

    distance calculate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [currentLocation, setCurrentLocation] = useState(null);
+  const [distance,setDistance] = useState()
+
+  function calculateDistance(origin, destination) {
+    const earthRadius = 6371; // Radius of the Earth in kilometers
+
+    const { lat: lat1, lng: lng1 } = origin;
+    const { lat: lat2, lng: lng2 } = destination;
+
+    const dLat = toRadians(lat2 - lat1);
+    const dLng = toRadians(lng2 - lng1);
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(toRadians(lat1)) *
+        Math.cos(toRadians(lat2)) *
+        Math.sin(dLng / 2) *
+        Math.sin(dLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadius * c;
+
+    return distance * 1000; // Convert distance to meters
+  }
+
+  function toRadians(degrees) {
+    return degrees * (Math.PI / 180);
+  }
 
   useEffect(() => {
     const watchId = navigator.geolocation.watchPosition(
@@ -10,10 +36,19 @@ const App = () => {
         setCurrentLocation({ latitude, longitude });
       },
       (error) => {
-        console.log('Error:', error);
+        console.log("Error:", error);
       },
       { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
     );
+
+    const distance = calculateDistance(
+      { lat: currentLocation?.latitude, lng: currentLocation?.longitude },
+      { lat: -1.7824086, lng: 37.6267237 }
+    );
+
+    console.log(distance)
+
+    setDistance(distance)
 
     return () => {
       navigator.geolocation.clearWatch(watchId);
@@ -24,6 +59,7 @@ const App = () => {
     <div>
       <p>Latitude: {currentLocation?.latitude}</p>
       <p>Longitude: {currentLocation?.longitude}</p>
+      <p>Distance: {distance}</p>
     </div>
   );
 };
